15c2f171b11c1a6344ddce2ea14f2afa6f8d313a
JuanMndzStgo <84043074+JuanMndzStgo@users.noreply.github.com>
2021-08-03 20:07:38 -0500

    semiterminado

---
+++ src/main/java/edu/ieu/ejemplorest/repositorios/UsuarioRepositorio.java
@@ -0,0 +1,11 @@
+package edu.ieu.ejemplorest.repositorios;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import edu.ieu.ejemplorest.entities.Usuario;
+
+public interface UsuarioRepositorio extends CrudRepository<Usuario, Integer> {
+	@Query("SELECT user FROM Usuario user WHERE user.nombre = ?1 ")
+	public Usuario findByNombre(String nombre);
+}
--- src/main/java/edu/ieu/ejemplorest/servicios/UsuarioServiceImp.java
+++ src/main/java/edu/ieu/ejemplorest/servicios/UsuarioServiceImp.java
@@ -1,20 +1,111 @@
 package edu.ieu.ejemplorest.servicios;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import edu.ieu.ejemplorest.entities.Usuario;
+import edu.ieu.ejemplorest.repositorios.UsuarioRepositorio;
 
-public interface UsuarioServiceImp {
-	//consulta SELECT 
-	Usuario findById(long id);
-    Usuario findByNombre(String name);
-    List<Usuario> findAll(); 
-    boolean isUserExist(Usuario user);
-    //CREAR 
-    void saveUser(Usuario user);
-     //Actualizar
-    void updateUser(Usuario user);
-    //Borrar
-    void deleteUserById(long id);
+@Service
+public class UsuarioServiceImp implements UserService{
+	@Autowired
+	private UsuarioRepositorio userDao;
+	
+	//	private static final AtomicLong counter = new AtomicLong();
+//    private static List<Usuario> users;
+//	
+//    static{
+//        users = populateDummyUsers();
+//    }
+//    
+//	private static List<Usuario> populateDummyUsers() {
+//		List<Usuario> users = new ArrayList<Usuario>();
+//        users.add(new Usuario( (int)counter.incrementAndGet(),"sam@email.com","sam", "Sam", "Simpson"));
+//        users.add(new Usuario( (int)counter.incrementAndGet(),"tom@email.com", "tom", "Tom", "Smith"));
+//        users.add(new Usuario( (int)counter.incrementAndGet(),"jrome@email.com","jor", "Jerome", "Blatzi"));
+//        users.add(new Usuario( (int)counter.incrementAndGet(),"silvia@email","sil", "Silvia", "Romero"));
+//        return users;
+}
+    
+	@Override
+	public Usuario findById(long id) {
+		return userDao.findById((int)id).orElse(null);
+//Optional<Usuario> userDao.findById(id);
+		
+//		if (opt.isPresent()) {
+//		return opt.get();
+//		}else {
+//			return null;
+//		for(Usuario user : users){
+//            if(user.getId() == id){
+//                return user;
+//            }
+//        }
+//        return null;
+	}
 
+	@Override
+	public Usuario findByNombre(String name) {
+		userDao.findByNombre(name);
+		return userDao.findByNombre(name);
+//		for(Usuario user : users){
+//            if(user.getNombre().equalsIgnoreCase(name)){
+//                return user;
+//            }
+//        }
+   //     return null;
+	}
+
+	@Override
+	public List<Usuario> findAll() {
+		List<Usuario> lista = new ArrayList<>();
+		userDao.findAll()
+		.forEach(lista::add	);
+			return lista;
+		//	return users;
+	}
+
+	@Override
+	public boolean isUserExist(Usuario user) {	
+		return userDao.existsById(user.getId());
+		//return findByNombre(user.getNombre()) != null;
+	}
+
+	@Override
+	public void saveUser(Usuario user) {
+		userDao.save(user);
+//		user.setId((int) counter.incrementAndGet());
+//        users.add(user);
+//		
+	}
+
+	@Override
+	public void updateUser(Usuario user) {
+		Usuario userdb = userDao.findById(user.getId()).orElse(null);
+		if(userdb!= null) {
+			userdb.setApellidos(user.getApellidos());
+			userdb.setEmail(user.getEmail());
+			userdb.setNombre(user.getNombre());
+			userdb.setPassword(user.getPassword());
+			userDao.save(user);
+//		int index = users.indexOf(user);
+//        users.set(index, user);
+	}
+
+	@Override
+	public void deleteUserById(long id) {
+//		for (Iterator<Usuario> iterator = users.iterator(); iterator.hasNext(); ) {
+//            Usuario user = iterator.next();
+//            if (user.getId() == id) {
+//                iterator.remove();
+//            }
+//        }		
+	}
+	
 }
