d5daf60e1be09973dbe2ae6707b8c97c54cf3bd9
JuanMndzStgo <84043074+JuanMndzStgo@users.noreply.github.com>
2021-08-10 20:15:14 -0500

    actualizado

--- src/main/java/edu/ieu/ejemplorest/servicios/UsuarioServiceImp.java
+++ src/main/java/edu/ieu/ejemplorest/servicios/UsuarioServiceImp.java
@@ -4,22 +4,22 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import edu.ieu.ejemplorest.entities.Usuario;
 import edu.ieu.ejemplorest.repositorios.UsuarioRepositorio;
 
 @Service
 public class UsuarioServiceImp implements UserService{
+	
 	@Autowired
 	private UsuarioRepositorio userDao;
-	
-	//	private static final AtomicLong counter = new AtomicLong();
+//	private static final AtomicLong counter = new AtomicLong();
 //    private static List<Usuario> users;
-//	
+	
 //    static{
 //        users = populateDummyUsers();
 //    }
@@ -31,17 +31,19 @@ public class UsuarioServiceImp implements UserService{
 //        users.add(new Usuario( (int)counter.incrementAndGet(),"jrome@email.com","jor", "Jerome", "Blatzi"));
 //        users.add(new Usuario( (int)counter.incrementAndGet(),"silvia@email","sil", "Silvia", "Romero"));
 //        return users;
-}
+//	}
     
 	@Override
 	public Usuario findById(long id) {
-		return userDao.findById((int)id).orElse(null);
-//Optional<Usuario> userDao.findById(id);
+		return userDao.findById((int) id)
+				.orElse(null);
 		
-//		if (opt.isPresent()) {
-//		return opt.get();
+//		Optional<Usuario> opt = userDao.findById(id);
+//		if(opt.isPresent()) {
+//			return opt.get();
 //		}else {
 //			return null;
+//		}
 //		for(Usuario user : users){
 //            if(user.getId() == id){
 //                return user;
@@ -52,54 +54,65 @@ public class UsuarioServiceImp implements UserService{
 
 	@Override
 	public Usuario findByNombre(String name) {
-		userDao.findByNombre(name);
 		return userDao.findByNombre(name);
+		
 //		for(Usuario user : users){
 //            if(user.getNombre().equalsIgnoreCase(name)){
 //                return user;
 //            }
 //        }
-   //     return null;
+//        return null;
 	}
 
 	@Override
 	public List<Usuario> findAll() {
 		List<Usuario> lista = new ArrayList<>();
 		userDao.findAll()
-		.forEach(lista::add	);
-			return lista;
-		//	return users;
+			.forEach( lista::add );
+		return lista;
+		
+//		 Iterable<Usuario> iter =userDao.findAll();
+//		 Iterator<Usuario> it = iter.iterator();
+//		 while(it.hasNext())
+//		 {
+//			 lista.add( it.next());
+//		 }
+		
+//		return users;
 	}
 
 	@Override
-	public boolean isUserExist(Usuario user) {	
-		return userDao.existsById(user.getId());
-		//return findByNombre(user.getNombre()) != null;
+	public boolean isUserExist(Usuario user) {
+		return (userDao.findByNombre(user.getNombre() ) != null);
+//		return findByNombre(user.getNombre()) != null;
 	}
 
-	@Override
+	@Override @Transactional
 	public void saveUser(Usuario user) {
 		userDao.save(user);
 //		user.setId((int) counter.incrementAndGet());
 //        users.add(user);
-//		
+		
 	}
 
-	@Override
+	@Override @Transactional
 	public void updateUser(Usuario user) {
-		Usuario userdb = userDao.findById(user.getId()).orElse(null);
-		if(userdb!= null) {
-			userdb.setApellidos(user.getApellidos());
-			userdb.setEmail(user.getEmail());
-			userdb.setNombre(user.getNombre());
-			userdb.setPassword(user.getPassword());
-			userDao.save(user);
+		Usuario userdb = userDao.findById(user.getId() ).orElse(null);
+		if(userdb != null) {
+			userdb.setApellidos( user.getApellidos() );
+			userdb.setEmail( user.getEmail() );
+			userdb.setNombre( user.getNombre() );
+			userdb.setPassword( user.getPassword() );
+			userDao.save(userdb);
+		}
 //		int index = users.indexOf(user);
 //        users.set(index, user);
 	}
 
-	@Override
+	@Override @Transactional
 	public void deleteUserById(long id) {
+		userDao.deleteById( (int) id );
+		
 //		for (Iterator<Usuario> iterator = users.iterator(); iterator.hasNext(); ) {
 //            Usuario user = iterator.next();
 //            if (user.getId() == id) {
